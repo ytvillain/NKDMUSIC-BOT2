38fe7dbafa06c4bbffea08dd149d704065db9e14
GREATPERSON <168116588+greatpersonxd@users.noreply.github.com>
2024-05-09 09:24:30 +0530

    Update thumbnails.py

--- ANNIEMUSIC/utils/thumbnails.py
+++ ANNIEMUSIC/utils/thumbnails.py
@@ -121,7 +121,7 @@ async def get_thumb(videoid):
             font4 = ImageFont.truetype('ANNIEMUSIC/assets/thumb/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "JENNY x MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((10, 10), "DOREAMON x MUSIC", fill="white", font = font1, align ="left") 
             image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
