4f010637729abd447c6c1d89a64ac0ac02a9a912
ytvillain <moviefdas07@gmail.com>
2024-05-26 17:08:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(getenv("OWNER_ID", 5730490876))
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Ytrolex07/NKD_music-Bot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ytvillain/NKDMUSIC-BOT2")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
